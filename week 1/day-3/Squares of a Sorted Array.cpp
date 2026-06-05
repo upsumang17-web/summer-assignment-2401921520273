3e80abfe0935b832d3e8c51c0a047e4bfce197b9
upsumang17-web <upsumang17@gmail.com>
2026-06-05 13:12:33 +0530

    Update Squares of a Sorted Array.cpp

--- week 1/day-3/Squares of a Sorted Array.cpp
+++ week 1/day-3/Squares of a Sorted Array.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>ans;
+        int i=0;
+        while(i<nums.size()){
+            ans.push_back(nums[i]*nums[i]);
+            i++;
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
